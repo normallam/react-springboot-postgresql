b66c22f056d4e26b3cf367237260fcdbc334aeae
norm <normmron@naver.com>
2024-10-22 15:40:04 +0900

    test

---
+++ src/main/java/com/example/test/ATestApplication.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ATestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ATestApplication.class, args);
+	}
+
+}
