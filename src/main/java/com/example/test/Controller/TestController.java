1da5825ea06abea90bdd7aa03a09762d35cf5dd4
norm <normmron@naver.com>
2024-10-24 14:09:14 +0900

    React-Springboot-postgresql 연동 완료

--- src/main/java/com/example/test/Controller/TestController.java
+++ src/main/java/com/example/test/Controller/TestController.java
@@ -1,15 +1,26 @@
 package com.example.test.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.test.service.TestService;
+import com.example.test.entity.TestEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class TestController {
 
-    @GetMapping("/api/hello")
-    public String test(){
-        return "Hello, world!";
+    private TestService testService;
+
+    @Autowired
+    public TestController(TestService testService){
+        this.testService = testService;
     }
 
+    @GetMapping("/tests")
+    public TestEntity createTestEntity() {
+        return testService.createTestEntityRecord();
+    }
 }
 /*---------------------------------------------------테스트를 위한 텍스트 입력------------------------------------------------*/
